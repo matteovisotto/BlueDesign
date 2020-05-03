1ddaeaaa149861af3ac8241e16b2ac75aca7be8e
Matteo Visotto <matteo.visotto@mail.polimi.it>
2020-05-03 19:16:24 +0200

    Initial Commit

---
+++ BlueDesign/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlueDesign
+//
+//  Created by Matteo Visotto on 03/05/2020.
+//  Copyright © 2020 Matteo Visotto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
